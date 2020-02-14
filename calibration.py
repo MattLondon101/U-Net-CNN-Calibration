51a55ae171adb5dce282b6a8fa961a443150e70e
Matt London <matthewlondon101@gmail.com>
2020-02-14 02:32:42 -0500

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -4,3 +4,11 @@
 # FP = False Positive
 
 # Count non-zero pixels to determine total prediction pixels in image
+src='Path/to/images/'
+j=[]
+for fileName in natsorted(os.listdir(src),alg=ns.PATH):
+    filePath=os.path.join(src,fileName)
+    if os.path.isfile(filePath):
+        im=cv.imread(filePath,0)
+        nonzero = cv.countNonZero(im)
+        
